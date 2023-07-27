e44d289431c60a2bc91e17b97999b809b064328e
bububuu <albusor@gmail.com>
2023-07-27 09:40:27 +0200

    images add

--- src/app/pages/fruta/fruta.module.ts
+++ src/app/pages/fruta/fruta.module.ts
@@ -8,12 +8,22 @@ import { FrutaPageRoutingModule } from './fruta-routing.module';
 
 import { FrutaPage } from './fruta.page';
 
+import { SearchbarModule } from 'src/app/components/searchbar/searchbar.module';
+import { ProductCardModule } from 'src/app/components/product-card/product-card.module';
+import { CategoryItemModule } from 'src/app/components/category-item/category-item.module';
+import { HeaderPagesModule } from 'src/app/components/header-pages/header-pages.module';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FrutaPageRoutingModule
+    FrutaPageRoutingModule,
+    SearchbarModule,
+    ProductCardModule,
+    CategoryItemModule,
+    HeaderPagesModule
   ],
   declarations: [FrutaPage]
 })
--- src/app/pages/todos/todos.module.ts
+++ src/app/pages/todos/todos.module.ts
@@ -7,13 +7,21 @@ import { IonicModule } from '@ionic/angular';
 import { TodosPageRoutingModule } from './todos-routing.module';
 
 import { TodosPage } from './todos.page';
+import { SearchbarModule } from 'src/app/components/searchbar/searchbar.module';
+import { ProductCardModule } from 'src/app/components/product-card/product-card.module';
+import { CategoryItemModule } from 'src/app/components/category-item/category-item.module';
+import { HeaderPagesModule } from 'src/app/components/header-pages/header-pages.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TodosPageRoutingModule
+    TodosPageRoutingModule,
+    SearchbarModule,
+    ProductCardModule,
+    CategoryItemModule,
+    HeaderPagesModule
   ],
   declarations: [TodosPage]
 })
