c6afe3c50e3e1053a89890e88570297a1704b9c0
hsara595 <hsara595@gmail.com>
2023-07-27 10:36:40 +0200

    add login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'planeta-verde-80166',
+    appId: '1:731994803930:web:0014ae02091ef34f41dfe3',
+    storageBucket: 'planeta-verde-80166.appspot.com',
+    apiKey: 'AIzaSyBYEixrDeY9iKnce1kRJIartWV4j-dOP0Y',
+    authDomain: 'planeta-verde-80166.firebaseapp.com',
+    messagingSenderId: '731994803930',
+    measurementId: 'G-ZMN832JH8C',
+  },
+    production: false
 };
 
 /*
